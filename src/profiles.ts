aa234f541976ee02bfdf6cf3abf503f981828e30
Stephen Fraser <stephen.fraser@digirati.com>
2020-04-12 20:14:52 +0100

    Added missing, but non-standard profiles

--- src/profiles.ts
+++ src/profiles.ts
@@ -44,7 +44,14 @@ export const IIIF_3_IMAGE_LEVEL_0 = 'level0';
 export const IIIF_3_IMAGE_LEVEL_1 = 'level1';
 export const IIIF_3_IMAGE_LEVEL_2 = 'level2';
 
+// Non-standard
+export const IIIF_2_IMAGE_LEVEL_0_NO_JSON = 'http://iiif.io/api/image/2/level0';
+export const IIIF_2_IMAGE_LEVEL_1_NO_JSON = 'http://iiif.io/api/image/2/level1';
+export const IIIF_2_IMAGE_LEVEL_2_NO_JSON = 'http://iiif.io/api/image/2/level2';
+
 export const level1Support = [
+  IIIF_2_IMAGE_LEVEL_1_NO_JSON,
+  IIIF_2_IMAGE_LEVEL_2_NO_JSON,
   STANFORD_IIIF_IMAGE_COMPLIANCE_1,
   STANFORD_IIIF_IMAGE_COMPLIANCE_2,
   STANFORD_IIIF_IMAGE_CONFORMANCE_1,
@@ -66,6 +73,9 @@ export const level1Support = [
 ];
 
 export const imageServiceProfiles = [
+  IIIF_2_IMAGE_LEVEL_0_NO_JSON,
+  IIIF_2_IMAGE_LEVEL_1_NO_JSON,
+  IIIF_2_IMAGE_LEVEL_2_NO_JSON,
   STANFORD_IIIF_IMAGE_COMPLIANCE_0,
   STANFORD_IIIF_IMAGE_COMPLIANCE_1,
   STANFORD_IIIF_IMAGE_COMPLIANCE_2,
